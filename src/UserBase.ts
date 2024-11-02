92a64555e9874e0e145a98a261595fa9fb98b807
Marek Vorp <marek.vorp@gmail.com>
2024-11-02 10:42:36 +0200

    [New feature] | added UserEstonia, UserLatvia, UserBase, KYC classes and BaseUser tests

---
+++ src/UserBase.ts
@@ -0,0 +1,24 @@
+export class UserBase {
+  name: string
+  surname: string
+  age: number
+  personalCode: string
+  phone: string
+  address: string
+
+  constructor(
+    name: string,
+    surname: string,
+    age: number,
+    personalCode: string,
+    phone: string,
+    address: string,
+  ) {
+    this.name = name
+    this.surname = surname
+    this.age = age
+    this.personalCode = personalCode
+    this.phone = phone
+    this.address = address
+  }
+}
---
+++ src/UserEstonia.ts
@@ -0,0 +1,17 @@
+import { UserBase } from './UserBase'
+
+export class UserEstonia extends UserBase {
+  mobileIDAuthorization: undefined | boolean
+
+  constructor(
+    name: string,
+    surname: string,
+    age: number,
+    personalCode: string,
+    phone: string,
+    address: string,
+  ) {
+    super(name, surname, age, personalCode, phone, address)
+    this.mobileIDAuthorization = undefined
+  }
+}
