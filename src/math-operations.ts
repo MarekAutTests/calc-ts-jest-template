45968fcf9c0348a1b4bbdd1b5dd275c91144596b
Marek Vorp <marek.vorp@gmail.com>
2024-11-02 08:02:40 +0200

    [New feature] | add User and Processor classes (Lesson 06)

--- src/math-operations.ts
+++ src/math-operations.ts
@@ -1,14 +1,13 @@
 export class MathOperations {
+  add(a: number, b: number): number {
+    return a + b
+  }
 
-    add(a: number, b: number): number {
-        return a + b
-    }
+  subtract(a: number, b: number): number {
+    return a - b
+  }
 
-    subtract(a: number, b: number): number {
-        return a - b
-    }
-
-    squaring(a: number): number {
-        return a * a
-    }
+  squaring(a: number): number {
+    return a * a
+  }
 }
--- test/math-operations.test.ts
+++ test/math-operations.test.ts
@@ -1,29 +1,29 @@
-import {MathOperations} from '../src/math-operations'
+import { MathOperations } from '../src/math-operations'
 
 describe('MathOperations Class testing', () => {
-    let mathOps: MathOperations
+  let mathOps: MathOperations
 
-    test('should add two positive numbers correctly', () => {
-        mathOps = new MathOperations()
-        expect(mathOps.add(2, 3)).toBe(5)
-    })
+  test('should add two positive numbers correctly', () => {
+    mathOps = new MathOperations()
+    expect(mathOps.add(2, 3)).toBe(5)
+  })
 
-    test('should add negative and positive numbers correctly', () => {
-        mathOps = new MathOperations()
-        expect(mathOps.add(-3, 3)).toBe(0)
-    })
+  test('should add negative and positive numbers correctly', () => {
+    mathOps = new MathOperations()
+    expect(mathOps.add(-3, 3)).toBe(0)
+  })
 })
 
 describe('MathOperations class testing method squaring', () => {
-    let mathOps: MathOperations
+  let mathOps: MathOperations
 
-    test('should squaring correctly positive number', () => {
-        mathOps = new MathOperations()
-        expect(mathOps.squaring(2)).toBe(4)
-    })
+  test('should squaring correctly positive number', () => {
+    mathOps = new MathOperations()
+    expect(mathOps.squaring(2)).toBe(4)
+  })
 
-    test('should squaring correctly negative number', () => {
-        mathOps = new MathOperations()
-        expect(mathOps.squaring(-2)).toBe(4)
-    })
+  test('should squaring correctly negative number', () => {
+    mathOps = new MathOperations()
+    expect(mathOps.squaring(-2)).toBe(4)
+  })
 })
